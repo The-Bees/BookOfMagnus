97535749f2d74a3de8a711a8fbb5439d4e0ebd05
bees <bloodbound@hotmail.com>
2019-06-06 22:16:48 +0100

    Draw graph as svg and render on web page.

--- src/bookofmagnus/core/views.py
+++ src/bookofmagnus/core/views.py
@@ -13,15 +13,35 @@ def index(request):
     graph = AGraph(directed=True)
 
     for book in books:
-        graph.add_node(book.title)
+        graph.add_node(book.id, label=book.title)
 
         if book.follows:
             for parent in book.follows.all():
-                graph.add_edge(parent.title, book.title)
+                graph.add_edge(parent.id, book.id)
 
     print(graph.string())
 
     graph.layout(prog='dot')
-    graph.draw('file.png')
-    context = {}
+    svg_graph = graph.draw(format="svg")
+    context = {"svg": svg_graph}
     return render(request, 'core/index.html', context)
+
+
+"""
+def index(request):
+
+    books = Book.objects.all()
+
+    dot = Digraph(format="svg")
+    for book in books:
+        dot.node(book.strid(), label='''<<TABLE><TR><TD><img src="#"/></TD></TR></TABLE>>''')
+
+        if book.follows:
+            for parent in book.follows.all():
+                dot.edge(parent.strid(), book.strid())
+
+    dot_svg = dot.pipe().decode('utf-8')
+
+    context = {"svg": dot_svg}
+    return render(request, 'core/index.html', context)
+"""

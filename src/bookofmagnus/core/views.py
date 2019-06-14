e4bf97dd9fd1875f636272f580a836f9410b0e6a
bees <bloodbound@hotmail.com>
2019-06-14 13:57:47 +0100

    Search by book, display with prequels.

--- src/bookofmagnus/core/views.py
+++ src/bookofmagnus/core/views.py
@@ -19,9 +19,25 @@ def index(request):
         books = Book.objects.filter(characters__name=character)
 
         # Get the books they follow on from
-        # might need a function that grabs books all the way back, recursively
-        # To begin with show these in a different shape - perhaps a square
-        #follow_books
+        prequels = get_prequels(books)
+
+        # Remove any prequels already in books
+        prequels = prequels.exclude(id__in=books)
+
+        # Draw all the nodes
+        for book in books:
+            graph.add_node(book.id, label=book.title)
+
+            if book.follows.exists():
+                for parent in book.follows.all():
+                    graph.add_edge(parent.id, book.id)
+
+        for book in prequels:
+            graph.add_node(book.id, label=book.title, shape="box", color="red")
+
+            if book.follows.exists():
+                for parent in book.follows.all():
+                    graph.add_edge(parent.id, book.id)
 
     else:
 
@@ -30,11 +46,12 @@ def index(request):
         for book in books:
             graph.add_node(book.id, label=book.title)
 
-            if book.follows:
+            if book.follows.exists():
                 for parent in book.follows.all():
                     graph.add_edge(parent.id, book.id)
 
     graph.layout(prog='dot')
+    #print(graph.string())
     svg_graph = graph.draw(format="svg")
 
     context = {
@@ -47,21 +64,28 @@ def index(request):
     return render(request, 'core/index.html', context)
 
 
-"""
-def index(request):
+def get_prequels(books):
+    """
+    Get all prequels recursively for a queryset of books
+    """
 
-    books = Book.objects.all()
+    def _get_prequels(books):
 
-    dot = Digraph(format="svg")
-    for book in books:
-        dot.node(book.strid(), label='''<<TABLE><TR><TD><img src="#"/></TD></TR></TABLE>>''')
+        prequels = Book.objects.none()
+        for book in books:
 
-        if book.follows:
-            for parent in book.follows.all():
-                dot.edge(parent.strid(), book.strid())
+            if book.follows.exists():
 
-    dot_svg = dot.pipe().decode('utf-8')
+                book_follows = book.follows.all()
 
-    context = {"svg": dot_svg}
-    return render(request, 'core/index.html', context)
-"""
+                # Add them to the prequels queryset
+                prequels |= book_follows
+
+                # See if they have any prequels
+                prequels |= _get_prequels(book_follows)
+
+        return prequels
+
+    prequels = _get_prequels(books)
+
+    return prequels
